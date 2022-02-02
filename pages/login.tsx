a0c5af5cb3143c6a41ccb6f2651372b624406966
Keyur Patel <ekeyur@gmail.com>
2022-02-02 17:23:53 -0500

    fixed error

--- pages/login.tsx
+++ pages/login.tsx
@@ -51,7 +51,7 @@ export default function Login() {
           <div className='mt-5'>
           {isSuccess ? <div className="alert alert-success">Magic Link Sent. Please click a link in your email to login.</div> : null}
           {isError ? (
-              <div className="alert alert-error">An error occurred: {error?.message}</div>
+              <div className="alert alert-error">There was an error. Please try again later</div>
             ) : null}
             </div>
         </div>
